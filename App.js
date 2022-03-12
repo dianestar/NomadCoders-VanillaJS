de3875012e9ba2f21f96c483c54289d5d4b1a233
dianestar <dianestar@naver.com>
2022-03-12 22:07:00 +0900

    [feat] LOGIN

---
+++ App.js
@@ -0,0 +1,38 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "userName";
+
+function paintGreetings(userName) {
+    greeting.innerText = `Hello ${userName}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+function onLoginSubmit(event) {
+    // 새로고침 방지
+    event.preventDefault();
+
+    // loginForm 숨기기
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    
+    // userName 저장
+    const userName = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, userName);
+
+    // greeting 보이기
+    paintGreetings(userName);
+}
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if (savedUserName === null) {
+    // loginForm 보이기
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}
+else {
+    // greeting 보이기
+    paintGreetings(savedUserName);
+}
